a91fbabec3adcdf8f234b039b303f510f1f887e1
Kavia AI <production@kavia.ai>
2025-10-01 13:37:36 +0000

    CheckPoint - cg9d6f2ee8

--- mongodb_dashboard_backend/src/middleware/security.js
+++ mongodb_dashboard_backend/src/middleware/security.js
@@ -52,7 +52,7 @@ function corsMiddleware() {
   whitelist.add('https://localhost:3000');
 
   // Preview environment frontend
-  whitelist.add('https://vscode-internal-17802-beta.beta01.cloud.kavia.ai:3000');
+  whitelist.add('https://vscode-internal-14820-beta.beta01.cloud.kavia.ai:3000');
 
 
   const allowCredentials =
