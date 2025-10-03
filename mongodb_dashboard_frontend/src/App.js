286c6e8644dd5d058290b1634713ce245f90b22a
Kavia AI <production@kavia.ai>
2025-10-03 08:52:30 +0000

    CheckPoint - cg9d6f2ee8

--- mongodb_dashboard_frontend/src/App.js
+++ mongodb_dashboard_frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import { DataProvider } from "./context/DataContext.jsx";
 
 // PUBLIC_INTERFACE
 export default function App() {
-  /** Root component rendering application routes wrapped with the centralized DataProvider. */
-  return (
-    <DataProvider>
-      <AppRoutes />
-    </DataProvider>
-  );
+  /** Root component rendering application routes. */
+  return <AppRoutes />;
 }
