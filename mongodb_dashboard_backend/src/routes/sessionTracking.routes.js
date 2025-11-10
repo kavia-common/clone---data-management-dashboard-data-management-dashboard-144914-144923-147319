666ba0c6ab481bb8815e531b365426efda074f12
Kavia AI <production@kavia.ai>
2025-11-10 14:41:12 +0000

    Checkpoint 46 - cma4c7140a

--- mongodb_dashboard_backend/src/routes/sessionTracking.routes.js
+++ mongodb_dashboard_backend/src/routes/sessionTracking.routes.js
@@ -3,6 +3,7 @@ const { asyncHandler } = require('../utils/http');
 const { parsePagination } = require('../utils/http');
 const SessionTracking = require('../models/sessionTracking.model');
 // const { buildTenantCrudController } = require('../controllers/crudFactory.tenant');
+const { buildCrudController } = require('../controllers/crudFactory');
 
 const router = express.Router();
 const controller = buildCrudController(SessionTracking, '-session_start');
