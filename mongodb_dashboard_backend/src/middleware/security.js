130b6e45c7943887c8b4c66a36c9645501846667
Kavia AI <production@kavia.ai>
2025-11-16 17:19:36 +0000

    Checkpoint 4 - cm6c6eda06

--- mongodb_dashboard_backend/src/middleware/security.js
+++ mongodb_dashboard_backend/src/middleware/security.js
@@ -79,7 +79,7 @@ function corsMiddleware() {
   // Preview environment frontends
   // whitelist.add('https://kavia-dashboard-kavia-dev.cloud.kavia.ai');
   // Explicitly allow VSCode internal preview origin used by frontend
-  whitelist.add('https://vscode-internal-12780-beta.beta01.cloud.kavia.ai:3000');
+  whitelist.add('https://vscode-internal-23943-beta.beta01.cloud.kavia.ai:3000');
   
 
 
