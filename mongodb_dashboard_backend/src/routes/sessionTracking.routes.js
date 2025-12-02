c5bb92cb5c86228967a5382d0a2156ee26c820e0
Kavia AI <production@kavia.ai>
2025-12-02 12:53:11 +0000

    Checkpoint 15 - cmf82f398b

--- mongodb_dashboard_backend/src/routes/sessionTracking.routes.js
+++ mongodb_dashboard_backend/src/routes/sessionTracking.routes.js
@@ -72,6 +72,7 @@ router.use((req, res, next) => {
 });
 
 const { getSessionTrackingAggregates, getSessionTrackingRaw } = require('../controllers/sessionTracking.analytics.controller');
+const { getServiceUsage } = require('../controllers/sessionTracking.services.controller');
 
 // Aggregation endpoint must be defined before the generic list endpoint to avoid shadowing query param handling
 /**
@@ -81,6 +82,8 @@ const { getSessionTrackingAggregates, getSessionTrackingRaw } = require('../cont
  * Query: interval=(daily|weekly|monthly|custom), start, end
  * Returns: { data: [{ date, count }], meta: { interval, start, end, total } }
  */
+router.get('/services', asyncHandler(getServiceUsage));
+
 router.get(
   '/',
   sessionsEarlyBypassDetector,
